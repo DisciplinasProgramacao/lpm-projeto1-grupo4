1fc4543d1bf65ccb4b82311f6932c40611a42908
Vinicius Lima <thelimavinicius@gmail.com>
2023-03-08 22:20:57 -0300

    test(estoque): add venderProduto test; refactor adicionarEstoque test

--- codigo/src/test/java/EstoqueTest.java
+++ codigo/src/test/java/EstoqueTest.java
@@ -9,6 +9,14 @@ class EstoqueTest {
 
     Estoque estoque;
 
+    private Produto getProdutoByName(String nomeProduto) {
+        Optional<Produto> prodOpt = estoque.getProdutos().stream().filter(
+                p -> p.getNome().equals(nomeProduto)
+        ).findFirst();
+
+        return prodOpt.orElse(null);
+    }
+
     @BeforeEach
     void setUp() {
         estoque = new Estoque();
@@ -19,15 +27,16 @@ class EstoqueTest {
         assertAll(
                 () -> {
                     estoque.cadastrarProduto(new Produto("Coca-cola", "Refrigerante coca-cola 2L",6.0, 2.7, 40, 30));
-                    assertTrue(estoque.getProdutos().stream().anyMatch(p -> p.getNome().equals("Coca-cola")), "Deve haver um produto com nome 'Coca-cola' no estoque");
+                    assertTrue(
+                            estoque.getProdutos().stream().anyMatch(p -> p.getNome().equals("Coca-cola")),
+                            "Deve haver um produto com nome 'Coca-cola' no estoque"
+                    );
                 },
                 () -> {
                     estoque.cadastrarProduto(new Produto("Coca-cola", "Refrigerante coca-cola 2L",6.0, 2.7, 10, 30));
-                    Optional<Produto> prodOpt = estoque.getProdutos().stream().filter(p -> p.getNome().equals("Coca-cola")).findFirst();
-                    int qtd = prodOpt.isPresent() ? prodOpt.get().getQuantidade() : 0;
                     assertEquals(
                             50,
-                            qtd,
+                            getProdutoByName("Coca-cola").getQuantidade(),
                             "Deve adicionar 10 à quantidade de Coca-cola"
                     );
                 }
@@ -36,19 +45,53 @@ class EstoqueTest {
 
     @Test
     void testAdicionarEstoqueNoProduto() {
-        estoque.cadastrarProduto(new Produto("Coca-cola", "Refrigerante coca-cola 2L",6.0, 2.7, 40, 30));
-        estoque.adicionarEstoqueNoProduto("Coca-cola", 10);
-        Optional<Produto> prodOpt = estoque.getProdutos().stream().filter(p -> p.getNome().equals("Coca-cola")).findFirst();
-        int qtd = prodOpt.isPresent() ? prodOpt.get().getQuantidade() : 0;
-        assertEquals(
-                50,
-                qtd,
-                "Deve adicionar 10 à quantidade de Coca-cola"
+        assertAll(
+                () -> {
+                    estoque.cadastrarProduto(new Produto("Coca-cola", "Refrigerante coca-cola 2L",6.0, 2.7, 40, 30));
+                    estoque.adicionarEstoqueNoProduto("Coca-cola", 10);
+                    assertEquals(
+                            50,
+                            getProdutoByName("Coca-cola").getQuantidade(),
+                            "Deve adicionar 10 à quantidade de Coca-cola"
+                    );
+                },
+                () -> {
+                    assertThrows(
+                            IllegalArgumentException.class,
+                            () -> estoque.adicionarEstoqueNoProduto("Guarana", 10),
+                            "Deve lançar IllegalArgumentException pois Guarana não está cadastrado"
+                    );
+                }
         );
     }
 
     @Test
     void venderProduto() {
+        assertAll(
+                () -> {
+                    estoque.cadastrarProduto(new Produto("Coca-cola", "Refrigerante coca-cola 2L",6.0, 2.7, 40, 30));
+                    estoque.venderProduto("Coca-cola", 10);
+                    assertEquals(
+                            30,
+                            getProdutoByName("Coca-cola").getQuantidade(),
+                            "Deve ter vendido 10 unidades"
+                    );
+                },
+                () -> {
+                    assertThrows(
+                            IllegalArgumentException.class,
+                            () -> estoque.venderProduto("Coca-cola", 40),
+                            "Deve lançar IllegalArgumentException pois não há 40 Coca-cola"
+                    );
+                },
+                () -> {
+                    assertThrows(
+                            IllegalArgumentException.class,
+                            () -> estoque.venderProduto("Guarana", 10),
+                            "Deve lançar IllegalArgumentException pois Guarana não está cadastrado"
+                    );
+                }
+        );
     }
 
     @Test
