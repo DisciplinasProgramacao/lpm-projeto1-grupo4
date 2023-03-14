5155ab5e178ebb7a6a0372d74890cdd549370f9c
Rafael Pierre <pierredoc28@gmail.com>
2023-03-14 09:02:48 -0300

    cria documentacao controleDeVendas e Estoque

--- codigo/src/main/java/ControleDeVendas.java
+++ codigo/src/main/java/ControleDeVendas.java
@@ -2,23 +2,44 @@ public class ControleDeVendas {
     private Produto produto;
     private Integer itemsVendidos;
 
+    /**
+        Cria um objeto de controle de vendas para um determinado produto e quantidade de itens vendidos.
+        @param produto O objeto Produto que representa o produto vendido.
+        @param itemsVendidos O número de unidades do produto vendido.
+    */
     public ControleDeVendas(Produto produto, Integer itemsVendidos) {
         this.produto = produto;
         this.itemsVendidos = itemsVendidos;
     }
 
+    /**
+        Retorna o produto associado a esta venda.
+        @return o produto associado a esta venda
+    */
     public Produto getProduto() {
         return produto;
     }
 
+    /**
+        Define o produto que será vendido.
+        @param produto o produto a ser vendido
+    */
     public void setProduto(Produto produto) {
         this.produto = produto;
     }
 
+    /**
+        Retorna os itens vendidos associados a este produto.
+        @return o produto associado a esta venda
+    */
     public Integer getItemsVendidos() {
         return itemsVendidos;
     }
 
+    /**
+        Define o número de itens vendidos para esse produto.
+        @param itemsVendidos o número de itens vendidos
+    */
     public void setItemsVendidos(Integer itemsVendidos) {
         this.itemsVendidos = itemsVendidos;
     }
--- codigo/src/main/java/Estoque.java
+++ codigo/src/main/java/Estoque.java
@@ -22,11 +22,23 @@ public class Estoque {
         this.produtos = produtos;
     }
 
+    /**
+        Cadastra um novo produto ou atualiza a quantidade de um produto existente na lista de produtos.
+        Se o produto já existe na lista, atualiza a quantidade do produto existente com a quantidade do produto a ser adicionado.
+        Caso contrário, adiciona o novo produto na lista.
+        @param produto o objeto Produto a ser adicionado ou atualizado na lista de produtos.
+    */
     public void cadastrarProduto(Produto produto) {
         Optional<Produto> prodEncontrado  = produtos.stream().filter(p -> p.getNome().equals(produto.getNome())).findFirst();
         prodEncontrado.ifPresentOrElse(value -> value.setQuantidade(value.getQuantidade() + produto.getQuantidade()), () -> produtos.add(produto));
     }
 
+    /**
+    Adiciona uma determinada quantidade ao estoque de um produto com o nome especificado.
+        @param nomeProduto o nome do produto que terá o estoque atualizado.
+        @param quantidade a quantidade que será adicionada ao estoque do produto.
+        @throws IllegalArgumentException se o produto não existir na lista de produtos.
+    */
     public void adicionarEstoqueNoProduto(String nomeProduto, Integer quantidade) {
         Optional<Produto> prodEncontrado  = produtos.stream().filter(p ->p.getNome().equals(nomeProduto)).findFirst();
         prodEncontrado.ifPresentOrElse(value -> value.setQuantidade(value.getQuantidade() + quantidade), () -> {
@@ -35,6 +47,12 @@ public class Estoque {
         );
     }
 
+    /**
+        Realiza a venda de uma determinada quantidade de um produto com o nome especificado.
+        @param nomeProduto o nome do produto a ser vendido.
+        @param quantidade a quantidade a ser vendida.
+        @throws IllegalArgumentException se o produto não existir na lista de produtos ou se a quantidade disponível for insuficiente.
+    */
     public void venderProduto(String nomeProduto, Integer quantidade) {
         Optional<Produto> prodEncontrado  = produtos.stream().filter(p ->p.getNome().equals(nomeProduto)).findFirst();
         prodEncontrado.ifPresentOrElse(p -> {
@@ -49,12 +67,21 @@ public class Estoque {
         });
     }
 
+    /**
+        Adiciona uma venda ao controle de vendas para um produto especificado.
+        @param quantidade a quantidade vendida.
+        @param p o produto vendido.
+     */
     private void addVendaNoControleDeVendas(Integer quantidade, Produto p) {
         Optional<ControleDeVendas> vendas =  vendasProdutos.stream().filter(v -> v.getProduto().getNome().equals(p.getNome())).findFirst();
         vendas.ifPresentOrElse(v -> v.setItemsVendidos(v.getItemsVendidos() + quantidade), () -> vendasProdutos.add(new ControleDeVendas(p, quantidade)));
     }
 
-
+    /**
+        Gera um relatório de vendas para um produto com o nome especificado.
+        @param nomeProduto o nome do produto a ser analisado.
+        @return um objeto RelatoriosVendasDTO com as informações de venda do produto, ou null se o produto não tiver sido vendido.
+     */
     public RelatoriosVendasDTO gerarRelatorioDeVendas(String nomeProduto) {
         ControleDeVendas controleDeVendas  = vendasProdutos.stream().filter(v -> v.getProduto().getNome().equals(nomeProduto)).findFirst().orElse(null);
         if (nonNull(controleDeVendas)) {
@@ -64,6 +91,10 @@ public class Estoque {
         return null;
     }
 
+    /**
+        Gera e imprime um relatório de vendas para um produto com o nome especificado.
+        @param nomeProduto o nome do produto a ser analisado.
+     */
     public void imprimirRelatorioVendas(String nomeProduto) {
         ControleDeVendas controleDeVendas  = vendasProdutos.stream().filter(v -> v.getProduto().getNome().equals(nomeProduto)).findFirst().orElse(null);
         if (nonNull(controleDeVendas)) {
